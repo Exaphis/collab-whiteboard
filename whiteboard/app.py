12379d0b4becf2dc8aea23ba0626b82d354fc720
Exaphis <kevin@kevinniuwu.com>
2019-11-10 14:01:29 -0500

    Fix url_for for NGINX proxy redirects

--- whiteboard/app.py
+++ whiteboard/app.py
@@ -13,19 +13,19 @@ button_state = False
 plots = []
 
 # Taken from https://stackoverflow.com/questions/32132648/python-flask-and-jinja2-passing-parameters-to-url-for
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
+#
+#
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path,
+#                                      endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
 
 
 @app.route('/')
